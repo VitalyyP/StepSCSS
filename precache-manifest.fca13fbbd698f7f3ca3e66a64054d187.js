fc8fe16d7747ee6d1616e544891f3d2eb623c2fc
Vitalii Pikozh <wisart@gmail.com>
2022-09-13 23:33:59 +0300

    Updates

---
+++ precache-manifest.fca13fbbd698f7f3ca3e66a64054d187.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "106b650a6cc053d67ec7",
+    "url": "/StepSCSS/static/css/main.ea7a0334.chunk.css"
+  },
+  {
+    "revision": "106b650a6cc053d67ec7",
+    "url": "/StepSCSS/static/js/main.106b650a.chunk.js"
+  },
+  {
+    "revision": "1977a96c3b19cede03a3",
+    "url": "/StepSCSS/static/js/1.1977a96c.chunk.js"
+  },
+  {
+    "revision": "ed35a8eb4aeb99143b9f",
+    "url": "/StepSCSS/static/js/runtime~main.ed35a8eb.js"
+  },
+  {
+    "revision": "228d4ef18d5495981fe9c09179947ac4",
+    "url": "/StepSCSS/static/media/checkbox.228d4ef1.svg"
+  },
+  {
+    "revision": "fe96dba10497630dfaf9405eb6e666b6",
+    "url": "/StepSCSS/static/media/arrowdown.fe96dba1.svg"
+  },
+  {
+    "revision": "692bae8c3367557ed533ff377f6ec2b3",
+    "url": "/StepSCSS/static/media/starFilled.692bae8c.svg"
+  },
+  {
+    "revision": "bdc802420b6eef078ce2000a8bf432b9",
+    "url": "/StepSCSS/static/media/starOutlined.bdc80242.svg"
+  },
+  {
+    "revision": "20ddc780bbcdba3faf2d82754abe4c69",
+    "url": "/StepSCSS/static/media/Rubik-VariableFont_wght.20ddc780.ttf"
+  },
+  {
+    "revision": "ba8d154465f7fd15e2fc2ced6dceec90",
+    "url": "/StepSCSS/static/media/Inter-VariableFont_slnt,wght.ba8d1544.ttf"
+  },
+  {
+    "revision": "a98d89f2fe86d46d0efd57d5bf8c57d3",
+    "url": "/StepSCSS/index.html"
+  }
+];
